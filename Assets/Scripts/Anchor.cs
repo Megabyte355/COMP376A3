3050e09b3802b25a3f5079935c84d54d2e754684
Gary Chang <gary.chang.355@gmail.com>
2014-11-14 04:37:09 -0500

    Added wind effect

---
+++ Assets/Scripts/Anchor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anchor : MonoBehaviour
+{
+    WindEffect wind;
+    void Start()
+    {
+        wind = GameObject.FindGameObjectWithTag("Wind").GetComponent<WindEffect>();
+    }
+
+    void Update()
+    {
+        //rigidbody.velocity = wind.GetAddedVelocity();
+        rigidbody.AddForce(wind.GetAddedAcceleration(), ForceMode.Acceleration);
+    }
+}
---
+++ Assets/Scripts/WindEffect.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindEffect : MonoBehaviour
+{
+    [SerializeField]
+    bool randomizeDirection;
+    [SerializeField]
+    Vector3 windDirection;
+    [SerializeField]
+    float amplitude;
+    [SerializeField]
+    float period;
+    float time;
+    Vector3 addedAcceleration;
+
+
+    void Awake()
+    {
+        if(randomizeDirection)
+        {
+            windDirection = new Vector3(Random.Range(-100f, 100f), 0f, Random.Range(-100f, 100f)).normalized;
+        }
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        float acc = amplitude * Mathf.Sin(2 * Mathf.PI * (1/period) * time);
+        addedAcceleration = windDirection.normalized * acc;
+        if(time >= period)
+        {
+            time -= period;
+        }
+    }
+
+    public Vector3 GetAddedAcceleration()
+    {
+        return addedAcceleration;
+    }
+}
