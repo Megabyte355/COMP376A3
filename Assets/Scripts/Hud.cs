d88447fb165e21e7fdc99c52d500b54b2711d6f2
Gary Chang <gary.chang.355@gmail.com>
2014-11-23 06:32:04 -0500

    Added HUD

---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hud : MonoBehaviour
+{
+    [SerializeField]
+    TextMesh ProgressUI;
+    [SerializeField]
+    TextMesh ProgressUIShadow;
+    [SerializeField]
+    TextMesh ScoreUI;
+    [SerializeField]
+    TextMesh ScoreUIShadow;
+    [SerializeField]
+    TextMesh LivesUI;
+    [SerializeField]
+    TextMesh LivesUIShadow;
+
+    Progression progress;
+
+    void Start()
+    {
+        progress = GameObject.FindGameObjectWithTag(Tags.Progress).GetComponent<Progression>();
+    }
+
+    void Update()
+    {
+        UpdateProgressUI(progress.GetProgressPercent());
+        UpdateScoreUI(progress.GetScore());
+    }
+
+    void UpdateProgressUI(float percent)
+    {
+        float displayed = Mathf.Round(percent * 100);
+        ProgressUI.text = "Progress: " + displayed + "%";
+        ProgressUIShadow.text = "Progress: " + displayed + "%";
+    }
+
+    void UpdateScoreUI(int score)
+    {
+        ScoreUI.text = "Score: " + score;
+        ScoreUIShadow.text = "Score: " + score;
+    }
+}
